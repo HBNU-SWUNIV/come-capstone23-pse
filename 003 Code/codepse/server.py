e8119f64a53ab0baa4797247511cb80588c12885
hhhhh1hhhh <hhhhh_hhhh@naver.com>
2023-07-30 17:56:42 +0900

    코딩 테스트 기능 모듈화

--- 003 Code/codepse/server.py
+++ 003 Code/codepse/server.py
@@ -1,5 +1,4 @@
 import os
-import html
 from flask import (
     Flask,
     render_template,
@@ -8,16 +7,18 @@ from flask import (
 )
 from app.auth import auth
 from app.game import game
+from app.coding_test import coding_test
+
 from database.database import get_db_connection
-from app.gpt_api import get_feedback, generate_response
 from database.models import QList
-from app.compile import c_compile_code, python_run_code, cpp_compile_code, grade_code
+from app.gpt_api import get_feedback, generate_response
 from app.config import Config
 
 app = Flask(__name__, static_folder="app/static")
 
 app.register_blueprint(game)
 app.register_blueprint(auth)
+app.register_blueprint(coding_test)
 
 app.template_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "app", "templates")
 
@@ -27,93 +28,6 @@ app.secret_key = Config.SECRET_KEY  # session 연결을 위한 키
 @app.route("/")
 def home():
     return render_template("main.html")
-
-
-@app.route("/test_list")
-def test_list():
-    # 데이터베이스 연결
-    conn = get_db_connection()
-
-    # 데이터베이스에서 데이터 가져오기
-    q_list = conn.query(QList.q_id, QList.q_level, QList.q_name).all()
-
-    # 가져온 데이터를 html 파일에 전달하기..
-    return render_template("test_list.html", q_list=q_list)
-
-
-# 상세보기 페이지
-@app.route("/test/<int:q_id>")
-def test_view(q_id):
-    conn = get_db_connection()
-
-    # 해당 문제 정보 가져오기
-    q_info = conn.query(QList).filter(QList.q_id == q_id).first()
-
-    # 개행 문자를 <br> 태그로 변환
-    q_info.ex_print = q_info.ex_print.replace("\n", "<br>")
-
-    #  세션에 현재 질문 ID를 저장하며, /submit 라우터는 이 ID를 사용하여 데이터베이스에서 해당 질문의 정답을 가져옴.
-    session["q_id"] = q_id
-
-    return render_template("test.html", q_list=q_info)
-
-
-@app.route("/compile", methods=["POST"])
-def compile():
-    code = request.form.get("code")
-    language = request.form.get("language")
-
-    session["language"] = language  # 여기에서 언어 정보를 세션에 저장
-
-    if language == "python":
-        output_str = python_run_code(code)
-    elif language == "c":
-        output_str = c_compile_code(code)
-    elif language == "c++":
-        output_str = cpp_compile_code(code)
-
-    return output_str
-
-
-@app.route("/submit", methods=["POST"])
-def submit():
-    conn = get_db_connection()
-
-    code = request.form.get("code")
-    language = request.form.get("language")
-
-    session["language"] = language  # 여기에서 언어 정보를 세션에 저장
-
-    if language == "python":
-        output_str = python_run_code(code)
-    elif language == "c":
-        output_str = c_compile_code(code)
-    elif language == "c++":
-        output_str = cpp_compile_code(code)
-
-    q_info = conn.query(QList).filter(QList.q_id == session["q_id"]).first()
-    expected_output = q_info.answer
-
-    result = grade_code(output_str, expected_output)
-
-    return result  # 채점 결과를 반환
-
-
-@app.route("/answer")
-def answer():
-    conn = get_db_connection()
-
-    q_info = conn.query(QList).filter(QList.q_id == session["q_id"]).first()
-
-    # 세션의 언어 정보에 따라 C 언어 정답 코드 혹은 Python 정답 코드를 가져옴
-    if session["language"] == "c":
-        answer = html.escape(q_info.c_answer_code)
-    elif session["language"] == "python":
-        answer = html.escape(q_info.p_answer_code)
-    elif session["language"] == "c++":
-        answer = html.escape(q_info.cpp_answer_code)
-
-    return "<pre>" + answer + "</pre>"
 
 
 @app.route("/feedback")
