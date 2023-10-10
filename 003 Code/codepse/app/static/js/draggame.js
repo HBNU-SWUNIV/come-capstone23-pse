156059bc86a91370ec6cad22f7b853b7c8365e08
YUJEONGHONG <yudongs910@daum.net>
2023-10-10 18:05:44 +0900

    드래그,출력값 게임 화면 완료

--- 003 Code/codepse/app/static/js/draggame.js
+++ 003 Code/codepse/app/static/js/draggame.js
@@ -383,3 +383,10 @@ function startQuiz() {
           document.getElementById("leaderboard").innerHTML = leaderboardHTML;
       });
   }
+
+  const header = document.querySelector("header")
+
+window.addEventListener ("scroll", function(){
+    header.classList.toggle("sticky", window.scrollY > 200)
+});
+//이거는 뭐시냐면은 그 메인페이지 스크롤 내릴때 메뉴는 안없어지고 그대로 유지되는 고것 근데 쫌 간지나게 유지돼..
