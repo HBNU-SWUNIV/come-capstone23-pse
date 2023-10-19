dbd1dc63eb6c03f570b5b6965743a027424e9bbc
jaeyeonkk <rlawodus456@naver.com>
2023-10-19 10:25:07 +0900

    js주석 수정 중

--- 003 Code/codepse/app/static/js/typinggame.js
+++ 003 Code/codepse/app/static/js/typinggame.js
@@ -5,68 +5,76 @@ window.addEventListener ("scroll", function(){
 });
 // 메뉴바 스크롤 애니메이션
 
-  const languageTag = document.querySelector(".language span b");
-  const descriptionTag = document.querySelector(".code-description");
-  const typingText = document.querySelector(".typing-text p"),
-  inpField = document.querySelector(".wrapper .input-field"),
-  tryAgainBtn = document.querySelector(".content button"),
-  timeTag = document.querySelector(".timeLeft span b");
+  // DOM 요소들 선택
+  const languageTag = document.querySelector(".language span b"); // 언어 표시
+  const descriptionTag = document.querySelector(".code-description"); // 코드 설명
+  const typingText = document.querySelector(".typing-text p"), // 사용자가 타이핑할 텍스트
+  inpField = document.querySelector(".wrapper .input-field"), // 사용자 입력 필드
+  tryAgainBtn = document.querySelector(".content button"), // 다시 시도 버튼
+  timeTag = document.querySelector(".timeLeft span b"); // 남은 시간 표시
   
+  // 게임 관련 변수 초기화
+  let timer, // 타이머 변수
+    maxTime = 100, // 최대 시간 설정
+    timeLeft = maxTime, // 남은 시간
+    charIndex = 0; // 현재 문자의 인덱스
+    errors = 0; // 사용자 오류 횟수
+    isTyping = 0; // 사용자가 타이핑 중인지 여부
   
-  let timer,
-    maxTime = 100,
-    timeLeft = maxTime,
-    charIndex = errors = isTyping = 0;
-  
+  // 서버에서 타이핑 게임에 사용될 문단 로드
   function loadParagraph() {
-    fetch('/api/get_typinggame_questions')
-    .then(response => response.json())
+    fetch('/api/get_typinggame_questions') // 서버 API 호출
+    .then(response => response.json()) // 응답을 JSON 형태로 변환
     .then(data => {
-      const code = data.code.split("\n");  // split with '\n' to handle each line
-      const language = data.language;  // Get the language of the code
-      const description = data.description;
+      const code = data.code.split("\n");  // 코드 줄바꿈 분할
+      const language = data.language;  // 코드 언어 정보
+      const description = data.description; // 코드 설명
 
-      typingText.innerHTML = "";  // Clear previous code
-      languageTag.innerText = language;  // Set language
-      descriptionTag.innerText = description;  // Set description
-    
-    // Handle each line
+      typingText.innerHTML = "";  // 이전 코드 삭제
+      languageTag.innerText = language;  // 언어 표시
+      descriptionTag.innerText = description;  // 코드 설명 표시
+
+    // 코드의 각 줄을 처리
     code.forEach((line, lineIndex) => {
+      // 각 줄의 문자를 처리
       line.split("").forEach((char) => {
         let span = `<span data-line="${lineIndex}">${char === ' ' ? '&nbsp;' : char}</span>`;
         typingText.innerHTML += span;
       });
+      // 각 줄 끝에 줄바꿈 추가 (마지막 줄 제외)
       if(lineIndex < code.length-1) {
         typingText.innerHTML += '<span class="line-end" data-line="' + lineIndex + '">&#9166;</span><br>'; // add newline symbol after each line except the last one
       }
     });
   
+    // 첫 번째 문자에 활성 클래스 추가
     typingText.querySelectorAll("span")[0].classList.add("active");
+    // 키보드 누를 때와 텍스트 클릭 시 입력 필드에 포커스
     document.addEventListener("keydown", () => inpField.focus());
     typingText.addEventListener("click", () => inpField.focus());
   
-      inpField.value = ""; // 새 문단이 로드될 때마다 입력 필드를 초기화합니다.
+      inpField.value = ""; // 새 문단이 로드될 때마다 입력 필드 초기화
   });
   }
   
   
-  // Remove the handling of '\n' to '<br/>', it's unnecessary
+  // 주 로직을 담당하는 함수
   function initTyping() {
-    let characters = typingText.querySelectorAll("span");
-    let typedChar = inpField.value.split("")[charIndex];
+    let characters = typingText.querySelectorAll("span"); // 현재 타이핑할 문자들
+    let typedChar = inpField.value.split("")[charIndex]; // 사용자가 입력한 현재 문자
   
-    if (typedChar === '\n') { // if user typed newline
+    // 새 줄로 이동했는지 확인
+    if (typedChar === '\n') { 
       const currentLine = characters[charIndex].getAttribute('data-line');
       const nextChar = characters[charIndex + 1];
       const nextLine = nextChar ? nextChar.getAttribute('data-line') : null;
     
-      // If the next character is from the next line, allow to proceed.
-      // Otherwise, ignore the newline.
+      // 다음 문자가 다음 둘의 것이면 허용
       if (nextLine !== currentLine) {
-        // Jump over indentation (spaces) at the beginning of the line
+        // 줄의 시작의 공백 건너뜀
         while (characters[charIndex + 1] && characters[charIndex + 1].innerHTML == '&nbsp;') {
           charIndex++;
-          characters[charIndex].classList.add("correct"); // 공백 문자를 '올바르게' 타이핑했다고 표시합니다.
+          characters[charIndex].classList.add("correct"); // 공백 문자를 올바르게 타이핑했다고 표시
         }
       } else {
         errors++;
@@ -77,6 +85,7 @@ window.addEventListener ("scroll", function(){
     }
     
     if (charIndex < characters.length) {
+      // 시간 끝나면 게임 중지
       if (timeLeft <= 0) {
         clearInterval(timer);
         isTyping = false;
@@ -188,6 +197,4 @@ window.addEventListener ("scroll", function(){
     initTyping();
   });
   tryAgainBtn.addEventListener("click", resetTest);
-  
-  
   
